f3e61afcf5c3774a4461a6f5624761235823c0ae
mainfranc <igoromskiy@gmail.com>
2021-04-14 22:56:07 +0300

    commit for merge

--- organizer/serializers.py
+++ organizer/serializers.py
@@ -6,5 +6,16 @@ from .models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = [
+            'id',
+            'title',
+            'note',
+            'author',
+            'comments',
+            'note_status',
+            'importance_status',
+            'public_status',
+            'task_date_time',
+            'views'
+        ]
         read_only_fields = ('author', 'views', )
